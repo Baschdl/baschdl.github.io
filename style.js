848badcdb71458c7761983b5ff2a04ca5776e5d5
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2020-06-07 16:45:32 +0200

    Refactor in separate style.js and order by logical flow

---
+++ style.js
@@ -0,0 +1,17 @@
+function getSnowStyle() {
+    return `
+  #layer {
+    polygon-fill: #00d0ff;
+    polygon-opacity: 0.3;
+  }
+`
+}
+
+function getNoDataStyle() {
+    return `
+  #layer {
+    polygon-fill: #eb3a34;
+    polygon-opacity: 0.2;
+  }
+`
+}
